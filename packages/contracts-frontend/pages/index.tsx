aeda350a54b89b368407f6dfa104f892597ef033
Ryan Cole <ryan@askdesigners.eu>
2022-03-29 09:44:07 +0200

    add form

--- packages/contracts-frontend/pages/index.tsx
+++ packages/contracts-frontend/pages/index.tsx
@@ -1,9 +1,9 @@
-import NextHead from 'next/head' // can come out when using layout
-// import { Layout } from '@create-nft-dao/shared'
-// import { Minter } from '../components/Minter'
+// import NextHead from 'next/head' // can come out when using layout
+import { Layout } from '@create-nft-dao/shared'
+import { Minter } from '../components/Minter'
 import { Video } from '../components/Video'
 import { Center, Container, SimpleGrid, Grid, GridItem, Heading, Text, Flex, Image, Link, Button } from '@chakra-ui/react'
-// import { NavbarLinks } from '../components/NavbarLinks'
+import { NavbarLinks } from '../components/NavbarLinks'
 
 const Mint = () => {
   const layoutProps = {
@@ -12,17 +12,7 @@ const Mint = () => {
   }
 
   return (
-    <>
-      <NextHead>
-        <title>{layoutProps.title}</title>
-        <meta content={layoutProps.description} name="description" />
-        <meta property="og:type" content='website' />
-        <meta property="og:site_name" content={layoutProps.title} />
-        <meta property="og:description" content={layoutProps.description} />
-        <meta property="og:title" content={layoutProps.title} />
-        <meta name="twitter:title" content={layoutProps.title} />
-        <meta name="twitter:description" content={layoutProps.description} />
-      </NextHead>
+    <Layout customMeta={layoutProps} navbarLinks={NavbarLinks}>
       <Container maxWidth="container.xl" pb={'5rem'}> 
         <Container maxW="container.lg" pt={'3rem'}>
           <Flex alignItems={'center'} justifyContent='space-between'>
@@ -54,7 +44,7 @@ const Mint = () => {
             <Center>
               <Minter />
             </Center>
-          </Container> */}
+          </Container>
           
           <Container p={'5rem'} bg="salmon" rounded={10} mb="10rem" id="mint">
             <Heading as='h2' textAlign={'center'} mb="2rem" color="white" css={{'fontFamily': 'EB Garamond', 'fontSize': '2rem'}}>
@@ -69,7 +59,7 @@ const Mint = () => {
                 Join the Discord for updates
               </Button>
             </Center>
-          </Container>
+          </Container> */}
           
           <Heading as='h3' pb={'20px'} css={{'fontFamily': 'EB Garamond', 'fontSize': '2rem'}}>
             Meet the Drones
@@ -216,7 +206,7 @@ const Mint = () => {
           </Grid>
         </Container>
       </Container>
-    </>
+    </Layout>
   )
 }
 
