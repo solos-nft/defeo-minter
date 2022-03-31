1c2370a5b2574b011f3515e071ee1781f51efb57
Ryan Cole <ryan@askdesigners.eu>
2022-03-31 15:22:27 +0200

    fix button

--- packages/contracts-frontend/pages/index.tsx
+++ packages/contracts-frontend/pages/index.tsx
@@ -2,7 +2,7 @@
 import { Layout } from '@create-nft-dao/shared'
 import { Minter } from '../components/Minter'
 import { Video } from '../components/Video'
-import { Center, Container, SimpleGrid, Grid, GridItem, Heading, Text, Flex, Image, Link, Button } from '@chakra-ui/react'
+import { Center, Container, SimpleGrid, Grid, GridItem, Heading, Text, Flex, Image, Link } from '@chakra-ui/react'
 import { NavbarLinks } from '../components/NavbarLinks'
 
 const Mint = () => {
